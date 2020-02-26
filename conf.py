19aeb71b840aeaf9c4e773fb5d4581d6a79c99b1
Simon Latapie <garf@videolan.org>
2020-02-26 09:49:20 +0100

    conf.py: exclude python virtualenv directory from sphinx parsing

--- conf.py
+++ conf.py
@@ -42,7 +42,7 @@ templates_path = ['./templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build','.venv']
 
 
 # -- Options for HTML output -------------------------------------------------
